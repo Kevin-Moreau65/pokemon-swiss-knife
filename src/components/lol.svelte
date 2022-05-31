6e02254a489d8239e7db751a771f199c677a8821
Kevin-Moreau65 <79045771+Kevin-Moreau65@users.noreply.github.com>
2022-05-31 21:36:09 +0200

    fix 1

--- src/components/lol.svelte
+++ src/components/lol.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { iconState } from '../utils/interface';
-
+	//icon
 	export let name: string;
 	export let handleClick: (type: string) => any;
 	export let state: iconState;
